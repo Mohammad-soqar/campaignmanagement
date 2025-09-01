87d21d93c83a1f0c8a93aa190abb664ee0be221b
Mohammad Soqar Ahmad <mnsoqar1@gmail.com>
2025-09-01 11:28:00 +0300

    normal updates

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from "drizzle-kit";
+import 'dotenv/config';
+import type { Config } from 'drizzle-kit';
 
 export default {
-  schema: "./src/server/db/schema/**/*.ts",
-  out: "./src/server/db/migrations",
-  dialect: "postgresql",
+  schema: './src/server/db/schema/**/*.ts',
+  out: './src/server/db/migrations',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
